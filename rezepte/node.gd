b1904538c1e565be4a3af7a717b9a26749de9b3c
maxbruegge11 <heimzii990@gmail.com>
2025-04-03 18:28:44 +0200

    init

---
+++ rezepte/node.gd
@@ -0,0 +1,36 @@
+extends Node
+
+var a = "Erdbeere"
+var b = "Blaubeere"
+var c= "Eichel"
+var d= "Magnolie"
+var e= "Orangenthymian"
+var f= "Rosmarin"
+var g= "Sauerklee"
+var h= "Pfefferminze"
+var i= "Kaffeebohnen"
+var j= "Moos"
+var k= "Honigwabe"
+var l= "Brennessel"
+var m= "Salbei"
+var n= "Feenstaub"
+var o= "Froschaugen"
+var p= "Himbeeren"
+var q= "Sumpfwasser"
+var r= "Zimt"
+var s= "Kristall"
+var t= "Irrlicht"
+
+var blueten = [d,g]
+var strauecher = [e,f,h,j,l,m]
+var beeren = [a,b,p]
+var random = [c,i,k,n,o,q,r,s,t]
+
+func mixen(slot1: Node2D, slot2: Node2D, slot3: Node2D ) -> String:
+	 var mixenfertig = slot1.string_value
+	
+	
+	
+	
+	
+	return "a"
